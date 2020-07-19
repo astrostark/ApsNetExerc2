9d59d7057c8c9b73382884231a2030196ba02d77
Astro <tkluan@hotmail.com.br>
2020-07-19 03:24:39 -0300

    Atividade 2

---
+++ AspNetExerc2/AspNetExerc2/Controllers/EnderecoAPIController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetExerc2.DAL;
+using AspNetExerc2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetExerc2.Controllers
+{
+    [Route("api/Endereco")]
+    [ApiController]
+    public class EnderecoAPIController : ControllerBase
+    {
+        //
+        // Nome: Luan Baum Bastos
+        // Matrícula: 1711925
+        //
+
+        private readonly EnderecoDAO _enderecoDAO;
+        public EnderecoAPIController(EnderecoDAO enderecoDAO)
+        {
+            _enderecoDAO = enderecoDAO;
+        }
+
+        // GET: /api/Endereco/ListarEnderecos
+        [HttpGet]
+        [Route("ListarEnderecos")]
+        public IActionResult Listar()
+        {
+            return Ok(_enderecoDAO.Listar());
+        }
+
+        // GET: /api/Endereco/ListarEndereco/81730000
+        [HttpGet]
+        [Route("ListarEndereco/{cep}")]
+        public IActionResult Pesquisar(string cep)
+        {
+            return Ok(_enderecoDAO.Pesquisar(cep));
+        }
+
+        // POST: /api/Endereco/CadastrarEndereco
+        [HttpPost]
+        [Route("CadastrarEndereco")]
+        public IActionResult Cadastar(Endereco endereco)
+        {
+            _enderecoDAO.Cadastrar(endereco);
+            return Created("", endereco);
+        }
+
+        // PUT: /api/Endereco/AlterarEndereco
+        [HttpPut]
+        [Route("AlterarEndereco")]
+        public IActionResult Alterar(Endereco endereco)
+        {
+            _enderecoDAO.Alterar(endereco);
+            return Ok(endereco);
+        }
+
+        // DELETE: /api/Endereco/DeletarEndereco/2
+        [HttpDelete]
+        [Route("DeletarEndereco/{cepId}")]
+        public IActionResult Alterar(int cepId)
+        {
+            if (_enderecoDAO.Remover(cepId))
+            {
+                return Ok(cepId);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
--- AspNetExerc2/AspNetExerc2/DAL/EnderecoDAO.cs
+++ AspNetExerc2/AspNetExerc2/DAL/EnderecoDAO.cs
@@ -23,11 +23,34 @@ namespace AspNetExerc2.DAL
         {
             return _context.Enderecos.ToList();
         }
-
         public Endereco Pesquisar(string cep)
         {
             string cepFormatado = cep.Substring(0, 5) + "-" + cep.Substring(5);
             return _context.Enderecos.SingleOrDefault(endereco => endereco.Cep == cepFormatado);
+        }
+        public Endereco PesquisarId(int enderecoId)
+        {
+            return _context.Enderecos.SingleOrDefault(endereco => endereco.CepId == enderecoId);
+        }
+        public void Alterar(Endereco endereco)
+        {
+            _context.Enderecos.Update(endereco);
+            _context.SaveChanges();
+
+        }
+        public bool Remover(int enderecoId)
+        {
+            Endereco endereco = PesquisarId(enderecoId);
+            if (endereco != null)
+            {
+                _context.Enderecos.Remove(endereco);
+                _context.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
